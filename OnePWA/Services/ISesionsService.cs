92f952e430e72e3935fa18d7f52945272ff32358
Luis <211g0373@rcarbonifera.tecnm.mx>
2025-11-05 23:41:57 -0600

    x

--- OnePWA/Services/ISesionsService.cs
+++ OnePWA/Services/ISesionsService.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnePWA.Models;
+using OnePWA.Models.DTOs;
 
 namespace OnePWA.Services
 {
     public interface ISesionsService
     {
         IEnumerable<IGameSesion> Sesions { get; set; }
-    
-        int 
-    
+
+        string CreateSesion(ICreateSesionDTO sesionDTO, int idHost);
+
+        ISesionDTO JoinSesionByCode(string code,int id);
+
+        ISesionDTO GetSesionByCode();
+
+
+
+
     }
 }
