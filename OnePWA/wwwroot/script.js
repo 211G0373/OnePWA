110c4a3e6ae2907f72f80048f2f96bee54dcad25
211G0370 <211G0370@rcarbonifera.tecnm.mx>
2025-11-08 01:37:39 -0600

    subi carpeta con el login and styles

---
+++ OnePWA/wwwroot/script.js
@@ -0,0 +1,15 @@
+// toggle contra
+const passwordToggle = document.getElementById('passwordToggle');
+const passwordInput = document.getElementById('password');
+
+passwordToggle.addEventListener('click', () => {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    passwordToggle.textContent = type === 'password' ? '👁️' : '🙈';
+});
+
+// Toggle remember me
+const rememberToggle = document.getElementById('rememberToggle');
+rememberToggle.addEventListener('click', () => {
+    rememberToggle.classList.toggle('active');
+});
