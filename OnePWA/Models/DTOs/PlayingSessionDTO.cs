ef6a21403d46bb853b92133fe924caa151ce9852
Luis <211g0373@rcarbonifera.tecnm.mx>
2025-12-12 01:03:35 -0600

    #

--- OnePWA/Models/DTOs/PlayingSessionDTO.cs
+++ OnePWA/Models/DTOs/PlayingSessionDTO.cs
@@ -6,6 +6,7 @@ namespace OnePWA.Models.DTOs
         public string Name { get ; set ; }
         public int PlayerCount { get ; set ; }
         public int IdTurn { get ; set ; }
+        public string LastColor { get ; set ; }
         public IEnumerable<IPlayerDTO> Players { get ; set ; } = new List<IPlayerDTO>();
         public CardDTO LastCard { get; set; }
         public List<CardDTO> MyCards { get ; set ; } = new List<CardDTO>();
